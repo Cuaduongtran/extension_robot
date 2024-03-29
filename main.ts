44e7779b6f232abb182b10b34a0f64d7018fa79d
Cuaduongtran <31402669+Cuaduongtran@users.noreply.github.com>
2024-03-29 23:10:53 +0700

    Update main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-microbit_robot.init()
-basic.forever(function () {
-    microbit_robot.Serial_print("", makerbit.getUltrasonicDistance(DistanceUnit.CM))
-    basic.pause(1000)
-})
-makerbit.connectUltrasonicDistanceSensor(DigitalPin.P5, DigitalPin.P8)
