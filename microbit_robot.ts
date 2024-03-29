79759b327be6798fddc829715a994d05f3f01ed5
Cuaduongtran <31402669+Cuaduongtran@users.noreply.github.com>
2024-03-29 11:45:48 +0700

    Update microbit_robot.ts

--- microbit_robot.ts
+++ microbit_robot.ts
@@ -1,7 +1,7 @@
 /**
  * microbit_robot
  */
-//% weight=100 color=#0fbc11 icon=""
+//% weight=100 color=#f06c icon=""
 namespace microbit_robot {
     // Flag to indicate whether the ESP8266 was initialized successfully.
     let esp8266Initialized = false
